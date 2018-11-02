fc279f91beb097eb144d8995f9674906452b068a
Jordan Yu <jordanyu44@gmail.com>
2018-11-01 22:50:24 -0700

    Build out page UI and add styles

---
+++ index.js
@@ -0,0 +1,25 @@
+let displayOne = document.querySelector(
+  ".display-content-one"
+);
+let displayTwo = document.querySelector(
+  ".display-content-two"
+);
+let displayThree = document.querySelector(
+  ".display-content-three"
+);
+let displayFour = document.querySelector(
+  ".display-content-four"
+);
+
+let buttonOne = document.querySelector(
+  ".sidebar-section-top-left"
+);
+let buttonTwo = document.querySelector(
+  ".sidebar-section-top-right"
+);
+let buttonThree = document.querySelector(
+  ".sidebar-section-bottom-left"
+);
+let buttonFour = document.querySelector(
+  ".sidebar-section-bottom-right"
+);
